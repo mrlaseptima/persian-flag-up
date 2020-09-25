9eebfb615b216a2bfbd0f803bc69660e1b283720
mohamad amin <mr.laseptima@gmail.com>
2020-09-25 14:29:48 +0330

    update website configuration

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  siteMetadata: {
+    title: `Persian Flag Up`,
+    description: `  یه راه حل خوب  برای هرکی که زیر پرچم ایرانه`,
+    author: `Amin moslemi`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`],
   pathPrefix: "/persian-flag-up",
